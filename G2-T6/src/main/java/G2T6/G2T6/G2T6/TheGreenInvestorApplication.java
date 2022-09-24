baa51da9eda35413d4d7c25ff21e8056e7eab40e
yogesh130 <yogesh@resynctech.com>
2022-09-24 18:19:37 +0800

    Added Login Page and button implementation

--- G2-T6/src/main/java/G2T6/G2T6/G2T6/TheGreenInvestorApplication.java
+++ G2-T6/src/main/java/G2T6/G2T6/G2T6/TheGreenInvestorApplication.java
@@ -1,10 +1,17 @@
 package G2T6.G2T6.G2T6;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class TheGreenInvestorApplication {
+@Configuration
+@ComponentScan
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class TheGreenInvestorApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheGreenInvestorApplication.class, args);
---
+++ G2-T6/src/main/java/G2T6/G2T6/G2T6/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package G2T6.G2T6.G2T6.security;
+
+import G2T6.G2T6.G2T6.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**");
+        super.configure(web);
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(User.withUsername("user").password("{noop}123456").roles("USER").build());
+    }
+}
